84353905ef456ca2778692f98e8052cb0a5fed4c
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-01-25 16:36:11 -0300

    adiciona queda bolhas

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 const canhao = document.querySelector("#c");
-let angulo = -120;
+const bolhas = document.querySelectorAll(".bolha");
+let angulo = 0;
 canhao.style.transform = `rotate(${angulo}deg)`;
 
 document.addEventListener("keydown", quandoTeclaPressionada);
@@ -26,3 +27,25 @@ function quandoTeclaPressionada(evento) {
       break;
   }
 }
+
+window.setInterval(passo, 1000);
+
+function passo() {
+  for (let i = 0; i < bolhas.length; i++) {
+    const bolha = bolhas[i];
+    let top = bolha.style.top;
+    let left = 0;
+    if (top) {
+        
+        top = parseInt(bolha.style.top);
+        top = top + 20;
+    } else {
+        top = 0;
+        left = Math.floor(440 * Math.random());
+        bolha.style.left = `${left}px`;
+    }
+    bolha.style.top = `${top}px`;
+  }
+}
+
+  
