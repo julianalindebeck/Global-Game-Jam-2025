0643c7b7e2d4507bde5e639dead9136d765c5e2c
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-01-25 15:18:23 -0300

    refatora teclas

--- jogo.js
+++ jogo.js
@@ -2,19 +2,27 @@ const canhao = document.querySelector("#c");
 let angulo = -120;
 canhao.style.transform = `rotate(${angulo}deg)`;
 
-document.addEventListener("keydown", giraHorario);
-document.addEventListener("keydown", giraAntiHorario);
+document.addEventListener("keydown", quandoTeclaPressionada);
 
-
-function giraHorario(evento) {
-    if(evento.key!="d") return;
-    
+function giraHorario() {
   angulo = angulo + 20;
   canhao.style.transform = `rotate(${angulo}deg)`;
 }
 
-function giraAntiHorario(evento) {
-    if(evento.key!="a") return;
-    angulo = angulo - 20;
-    canhao.style.transform = `rotate(${angulo}deg)`;
+function giraAntiHorario() {
+  angulo = angulo - 20;
+  canhao.style.transform = `rotate(${angulo}deg)`;
+}
+
+function quandoTeclaPressionada(evento) {
+  switch (evento.key) {
+    case "a":
+      giraAntiHorario();
+      break;
+    case "d":
+      giraHorario();
+      break;
+    default:
+      break;
   }
+}
