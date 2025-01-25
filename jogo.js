882f14e161ea9e3f95a285eb87982b0b4d749092
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-01-25 17:04:04 -0300

    movimenta tiro

--- jogo.js
+++ jogo.js
@@ -1,5 +1,9 @@
 const canhao = document.querySelector("#c");
+const tiro = document.querySelector("#t");
 const bolhas = document.querySelectorAll(".bolha");
+
+//inicia elemento
+tiro.style.top = "320px";
 
 for (let i = 0; i < bolhas.length; i++) {
   const bolha = bolhas[i];
@@ -37,19 +41,28 @@ function quandoTeclaPressionada(evento) {
   }
 }
 
-window.setInterval(passo, 1000);
+const dt = 16/1000;
+window.setInterval(passo, dt);
 
 function passo() {
   for (let i = 0; i < bolhas.length; i++) {
     const bolha = bolhas[i];
-    let top = parseInt(bolha.style.top);
-    top = top + 20;
-    bolha.style.top = `${top}px`;
+    let top = parseFloat(bolha.style.top);
+    top = top + 50*dt;
     if (top > 340) {
-      let top = -120 + Math.floor(80 * Math.random());
-      let left = Math.floor(440 * Math.random());
-      bolha.style.left = `${left}px`;
-      bolha.style.top = `${top}px`;
+        top = -120 + Math.floor(80 * Math.random());
+        let left = Math.floor(440 * Math.random());
+        bolha.style.left = `${left}px`;
     }
+    bolha.style.top = `${top}px`;
+  }
+
+  let tiroTop = parseFloat(tiro.style.top);
+  tiroTop = tiroTop - 80*dt;
+  tiro.style.top = `${tiroTop}px`;
+  if (tiroTop < -15) {
+    tiro.style.top = "320px";
   }
 }
+
+
