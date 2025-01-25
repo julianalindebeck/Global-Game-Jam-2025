930f775d069dfb5543c2e90291a03a2f6de13099
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-01-25 15:12:58 -0300

    adiciona evento canhao

--- jogo.js
+++ jogo.js
@@ -1,3 +1,20 @@
 const canhao = document.querySelector("#c");
-let angulo=-120;
-canhao.style.transform=`rotate(${angulo}deg)`;
+let angulo = -120;
+canhao.style.transform = `rotate(${angulo}deg)`;
+
+document.addEventListener("keydown", giraHorario);
+document.addEventListener("keydown", giraAntiHorario);
+
+
+function giraHorario(evento) {
+    if(evento.key!="d") return;
+    
+  angulo = angulo + 20;
+  canhao.style.transform = `rotate(${angulo}deg)`;
+}
+
+function giraAntiHorario(evento) {
+    if(evento.key!="a") return;
+    angulo = angulo - 20;
+    canhao.style.transform = `rotate(${angulo}deg)`;
+  }
