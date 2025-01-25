b9af843ae613c066a7499671c1ceb5f1dfd2fbc8
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-01-25 17:30:53 -0300

    corrige tiro

--- jogo.js
+++ jogo.js
@@ -3,8 +3,15 @@ const tiro = document.querySelector("#t");
 const bolhas = document.querySelectorAll(".bolha");
 
 //inicia elemento
-tiro.style.top = "320px";
+const dt = 16/1000;
+let angulo = -90;
+const vTiro = 100;
+tiro.style.top = "305px";
+tiro.style.left = `${480/2 - 15/2}px`;
 
+let vTop = -vTiro*Math.sin(angulo/180*Math.PI);
+let vLeft = vTiro*Math.cos(angulo/180*Math.PI);
+debugger
 for (let i = 0; i < bolhas.length; i++) {
   const bolha = bolhas[i];
   let top = -120 + Math.floor(80 * Math.random());
@@ -13,19 +20,18 @@ for (let i = 0; i < bolhas.length; i++) {
   bolha.style.top = `${top}px`;
 }
 
-let angulo = 0;
-canhao.style.transform = `rotate(${angulo}deg)`;
-
+canhao.style.transform = `rotate(${angulo+90}deg)`;
+debugger
 document.addEventListener("keydown", quandoTeclaPressionada);
 
 function giraHorario() {
   angulo = angulo + 20;
-  canhao.style.transform = `rotate(${angulo}deg)`;
+  canhao.style.transform = `rotate(${angulo+90}deg)`;
 }
 
 function giraAntiHorario() {
   angulo = angulo - 20;
-  canhao.style.transform = `rotate(${angulo}deg)`;
+  canhao.style.transform = `rotate(${angulo+90}deg)`;
 }
 
 function quandoTeclaPressionada(evento) {
@@ -41,7 +47,6 @@ function quandoTeclaPressionada(evento) {
   }
 }
 
-const dt = 16/1000;
 window.setInterval(passo, dt);
 
 function passo() {
@@ -58,10 +63,18 @@ function passo() {
   }
 
   let tiroTop = parseFloat(tiro.style.top);
-  tiroTop = tiroTop - 80*dt;
+  let tiroLeft = parseFloat(tiro.style.left);
+
+  tiroTop = tiroTop - vTop*dt;
+  tiroLeft = tiroLeft + vLeft*dt;
   tiro.style.top = `${tiroTop}px`;
-  if (tiroTop < -15) {
-    tiro.style.top = "320px";
+  tiro.style.left = `${tiroLeft}px`;
+  if (tiroTop < -15 || tiroTop > 320 || tiroLeft<-15 ||tiroLeft>495) {
+    tiro.style.top = "305px";
+    tiro.style.left = `${480/2 - 15/2}px`;
+    vTop = -vTiro*Math.sin(angulo/180*Math.PI);
+    vLeft = vTiro*Math.cos(angulo/180*Math.PI);
+
   }
 }
 
